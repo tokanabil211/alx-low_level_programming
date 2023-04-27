db2a89acd1c67ccdc0f76b70f94a625397e5dad8
tokanabil211 <tokanabil211@gmail.com>
2023-04-27 12:39:38 -0700

    dd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,38 +1,25 @@
-#include "main.h"
-
+#include <unistd.h>
 
 /**
-
- * more_numbers - print more numbers
-
+ * more_numbers - prints 0-14 ten times
  */
-
-
 void more_numbers(void)
-
 {
+	int i, j;
 
-        int i, j;
-
-
-        for (i = 1; i <= 10; i++)
-
-        {
-
-                for (j = 0; j <= 14; j++)
-
-                {
-
-                        if (j >= 10)
-
-                                _putchar('1');
-
-                        _putchar (j % 10 + '0');
-
-                }
-
-                _putchar('\n');
-
-        }
-
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				/* prints tens digit */
+				write(1, "1", 1);
+			}
+			/* prints ones digit */
+			write(1, &"0123456789"[j % 10], 1);
+		}
+		/* prints newline */
+		write(1, "\n", 1);
+	}
 }
